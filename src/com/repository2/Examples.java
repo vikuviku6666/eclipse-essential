c7cd36724f9ce991165781d5266e93569ce6551e
Vinay <53383503+vikuviku6666@users.noreply.github.com>
2021-11-16 21:28:00 +0100

    first commit

---
+++ src/com/repository2/Examples.java
@@ -0,0 +1,12 @@
+package com.repository2;
+
+public class Examples {
+
+	// TODO Auto-generated method stub
+	public static void main(String[] args) {
+		String a = "Vinay";
+		System.out.println(a);
+
+	}
+
+}
